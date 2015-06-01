1f2073169c50b4d319606f4906b2ec2508aa587c
Myles Smith <smithm4949@gmail.com>
2015-06-01 12:01:02 -0400

    Add Static Pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,19 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title","Home | Myles Smith Sample Site"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title","Help | Myles Smith Sample Site"
+  end
+
+  test "should get about" do
+    get :about
+    assert_response :success
+    assert_select "title","About | Myles Smith Sample Site"
   end
 
 end
