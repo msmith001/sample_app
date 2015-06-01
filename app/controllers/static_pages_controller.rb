0ec561043e3a582e1014883a1f932b074ab80e20
Myles Smith <smithm4949@gmail.com>
2015-06-01 12:26:22 -0400

    Finish static pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,13 @@
 class StaticPagesController < ApplicationController
   def home
+    render :layout => 'static'
   end
 
   def help
+    render :layout => 'static'
   end
 
   def about
+    render :layout => 'static'
   end
 end
